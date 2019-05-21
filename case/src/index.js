124f81cbee96d314b6ee7dccde0060fa1426ad3b
Jose Quintana <joseluisquintana20@gmail.com>
2019-05-21 17:27:24 +0200

    ignore some file

---
+++ case/src/index.js
@@ -0,0 +1,13 @@
+console.log('a')
+
+const a = require('~/000A'); const a = require('~/111B')
+
+console.log('b')
+
+const c = require('~/222C')
+
+let c
+
+if c = require('~/333D') {
+    // nothing to do
+}
