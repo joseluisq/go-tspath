762dc8d722c233f21b5842979ad01007ec3d6303
Jose Quintana <joseluisquintana20@gmail.com>
2019-06-11 12:29:49 +0200

    chore: ts paths validation

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/joseluisq/go-tspath/pkg/replacer"
 	"github.com/joseluisq/go-tspath/pkg/tsconfig"
@@ -38,16 +39,63 @@ func Execute() {
 		os.Exit(1)
 	}
 
-	// TODO: create a replacement string array
+	// TODO: create the replacement string array (pattern-replacement)
 	var replacements []tsconfig.TSPathReplacement
+
+	for kPathStr, vPathStr := range config.CompilerOptions.Paths {
+		kParts := strings.Split(kPathStr, "/")
+
+		// 0. Prevent no valid paths (key-value)
+		if len(kParts) <= 0 || len(vPathStr) <= 0 {
+			continue
+		}
+
+		// 1. Pattern placeholder: Take key parts skipping the last one
+		patternStr := strings.TrimSpace(strings.Join(kParts[:len(kParts)-1], "/"))
+
+		if len(patternStr) <= 0 {
+			continue
+		}
+
+		// Store pattern string as byte
+		pattern := []byte(patternStr)
+
+		// 2. Replacement placeholder: Take value parts skipping the last one
+		replacementBytes := []byte(nil)
+
+		for _, vpathstr := range vPathStr {
+			vparts := strings.Split(vpathstr, "/")
+
+			// Prevent no valid replacement paths
+			if len(vparts) <= 0 {
+				continue
+			}
+
+			value := strings.TrimSpace(strings.Join(vparts[:len(vparts)-1], "/"))
+
+			// Prevent empty replacement paths
+			if len(value) <= 0 {
+				continue
+			}
+
+			replacementBytes = append(replacementBytes, []byte(value)...)
+		}
+
+		fmt.Println("replacement PatternStr:", string(pattern))
+		fmt.Println("replacement Replacement:", string(replacementBytes))
+
+		// TODO: append values to `replacements` slice
+	}
+
+	fmt.Println("-----")
 
 	// TODO: replace all ocurrences per file
 	for _, file := range files {
 		replacer.Replace(file, replacements)
 	}
 
-	fmt.Println(config)
-	fmt.Println("SOURCE_PATH:", *sourcePath)
-	fmt.Println("ABS_SOURCE_PATH:", absSourcePath)
-	fmt.Println("FILES:", files)
+	// fmt.Println(config.CompilerOptions.Paths)
+	// fmt.Println("SOURCE_PATH:", *sourcePath)
+	// fmt.Println("ABS_SOURCE_PATH:", absSourcePath)
+	// fmt.Println("FILES:", files)
 }
